ea6b6fe92bf191cfdff84029b386181efce631a6
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-02-12 23:49:25 +0530

    feat: build basic login page structure

--- web_sql/app/page.tsx
+++ web_sql/app/page.tsx
@@ -1,8 +1,45 @@
-export default function Home(){
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { link } from "fs"
+
+export default function Login(){
   return(
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page</p>
+    <div className="h-screen w-screen flex justify-center items-center">
+      
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Where every book is just a tap away.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="username">Username</Label>
+              <Input type="text" id="username" placeholder="Your Username" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" placeholder="Your Password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-4">
+        <Button className="w-full">Login</Button>
+        <Button variant="link">Don't have an account? Sign up</Button>
+      </CardFooter>
+    </Card>
+
     </div>
   )
 }
