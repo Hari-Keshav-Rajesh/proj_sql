206f19764264774c800b547f68a844b2dcf9421a
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-02-15 13:41:40 +0530

    feat: add basic header layout

---
+++ web_sql/components/icon-list.tsx
@@ -0,0 +1,76 @@
+import { siteConfig } from '@/config/siteconfig';
+
+import Link from 'next/link';
+
+import { Icons } from '@/config/icons';
+import { buttonVariants } from '@/components/ui/button';
+
+export default function IconList() {
+  return (
+    <nav className="flex items-center space-x-1">
+      <Link
+        href={siteConfig.mediaLinks.instagram}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div
+          className={buttonVariants({
+            size: 'icon',
+            variant: 'ghost',
+          })}
+        >
+          <Icons.instagram className="h-5 w-5" />
+          <span className="sr-only">Instagram</span>
+        </div>
+      </Link>
+
+      <Link
+        href={siteConfig.mediaLinks.twitter}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div
+          className={buttonVariants({
+            size: 'icon',
+            variant: 'ghost',
+          })}
+        >
+          <Icons.twitter className="h-5 w-5" />
+          <span className="sr-only">Twitter</span>
+        </div>
+      </Link>
+
+      <Link
+        href={siteConfig.mediaLinks.linkedin}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div
+          className={buttonVariants({
+            size: 'icon',
+            variant: 'ghost',
+          })}
+        >
+          <Icons.linkedin className="h-5 w-5 fill-current" />
+          <span className="sr-only">LinkedIn</span>
+        </div>
+      </Link>
+
+      <Link
+        href={siteConfig.mediaLinks.github}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div
+          className={buttonVariants({
+            size: 'icon',
+            variant: 'ghost',
+          })}
+        >
+          <Icons.github className="h-5 w-5" />
+          <span className="sr-only">GitHub</span>
+        </div>
+      </Link>
+    </nav>
+  );
+}
---
+++ web_sql/config/siteconfig.tsx
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    title: "INFINITI",
+    description: "Limitless knowledge,at your fingertips",
+    navLinks: [
+        { name: "Home", href: "/dash" },
+        { name: "Books", href: "/" },
+        { name: "Admin", href: "/" },
+    ],
+    mediaLinks: {
+        instagram: 'https://www.instagram.com',
+        linkedin: 'https://in.linkedin.com',
+        github: 'https://github.com',
+        twitter: 'https://twitter.com',
+    }
+}
