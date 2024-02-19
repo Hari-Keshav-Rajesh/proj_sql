25a59fac5a7313390440309e671b51af13e53f6e
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-02-19 08:35:58 +0530

    chore: add dashboard navigation

--- web_sql/config/siteconfig.tsx
+++ web_sql/config/siteconfig.tsx
@@ -3,13 +3,13 @@ export const siteConfig = {
     description: "Limitless knowledge,at your fingertips",
     navLinks: [
         { name: "Home", href: "/dash" },
-        { name: "Books", href: "/" },
-        { name: "Admin", href: "/" },
+        { name: "Books", href: "/dash/books" },
+        { name: "Personal", href: "/dash/personal" },
     ],
     mediaLinks: {
         instagram: 'https://www.instagram.com',
         linkedin: 'https://in.linkedin.com',
-        github: 'https://github.com',
+        github: 'https://github.com/Hari-Keshav-Rajesh/proj_sql',
         twitter: 'https://twitter.com',
     }
 }
