4170fe9fb1200023a2e207353d83a11f7f242dae
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-03-12 18:39:07 +0530

    chore: add title and FAQs to home

--- web_sql/app/dash/page.tsx
+++ web_sql/app/dash/page.tsx
@@ -1,9 +1,16 @@
 import BookCard from "@/components/bookCard";
 
+import Title from "@/components/dash/title";
+
+import FAQs from "@/components/dash/faq/faq";
+
 export default function home() {
   return (
-    <div className="flex justify-center items-center h-screen w-90">
-      <BookCard />
+    <div className="mt-32 flex flex-col gap-24">
+      <Title />
+      <div className="mx-20">
+        <FAQs />
+      </div>
     </div>
   );
 }
---
+++ web_sql/components/dash/faq/data.ts
@@ -0,0 +1,24 @@
+const data = [
+    {
+      Question: 'How to collect reserved books',
+      Answer:
+        'Books reserved on the site can be collected from the library by showing the reservation email to the librarian. The books will be available to the user for 15 days, during which they can collect and return the books at any time.',
+    },
+    {
+      Question: 'Is there late fee?',
+      Answer:
+        'Yes, there is a late fee of 5 rupees per day for each book that is not returned on time. The late fee is capped at 100 rupees per book',
+    },
+    {
+      Question: 'How to reserve a book?',
+      Answer:
+        'To reserve a book, the user must be logged in. Once logged in, the user can find a book they like and reserve it.',
+    },
+    {
+      Question: 'Is there a limit for reserving books?',
+      Answer:
+        'Yes,a user can reserve a maximum of 3 books at a time.',
+    },
+  ];
+  
+  export default data;
